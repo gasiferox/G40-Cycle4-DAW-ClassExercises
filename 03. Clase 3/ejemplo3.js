b364d56f168b067db741398a26fbe90394beaab1
Gustavo Romero <gasifero@gmail.com>
2021-11-01 12:39:45 -0500

    main -> mejora en las opciones de cada estado, se muestran las opciones correctas

--- 03. Clase 3/ejemplo3.js
+++ 03. Clase 3/ejemplo3.js
@@ -12,7 +12,7 @@ window.onload = function () {
 
         getEstados(seleccionado);
 
-        getCiudades(seleccionado);
+        /* getCiudades(seleccionado); */
 
         getBandera(seleccionado)
     })
@@ -24,14 +24,14 @@ window.onload = function () {
         if (seleccionado == "Venezuela")
             arrayEstados = ["Carabobo", "Anzoategui", "Zulia", "Apure", "Mérida"];
         if (seleccionado == "Estados Unidos")
-            arrayEstados = ["Florida", "Nevada", "Iowa", "Kentuky", "Colorado"]
+            arrayEstados = ["Florida", "Nevada", "Iowa", "Kentucky", "Colorado"]
 
         var estados = document.getElementById("estados");
         for (i = 0; i < arrayEstados.length; i++)
             estados.options[i+1].text = arrayEstados[i];
     }
 
-    function getCiudades(seleccionado) {
+    /* function getCiudades(seleccionado) {
         var arrayCiudades = [];
         if (seleccionado == "Colombia")
             arrayCiudades = ["Bogotá", "Medellin", "Cali", "Cartagena", "Pereira"];
@@ -43,6 +43,94 @@ window.onload = function () {
         var ciudades = document.getElementById("ciudades");
         for (i = 0; i < arrayCiudades.length; i++)
             ciudades.options[i].text = arrayCiudades[i];
+    } */
+
+    document.getElementById("estados").addEventListener("change", () => {
+        var estados = document.getElementById("estados");
+
+        var estadoSel = estados.options[estados.selectedIndex].text;
+
+        console.log("el estado seleccionado es " + estadoSel)
+
+        getCiudades(estadoSel);
+    })
+
+    function getCiudades(estadoSel)
+    {
+        var arrayCiudades = [];
+        /* Colombia */
+        if (estadoSel == "Cundinamarca") {
+            arrayCiudades = ["Bogotá", "Sopó", "Chia", "Cajicá", "Cucunubá"];
+        }
+        if (estadoSel == "Boyacá") {
+            arrayCiudades = ["Tunja", "Duitama", "Paipa", "Sogamoso", "Villa de Leyva"];
+            console.log("dentro del if, " + estadoSel)
+        }
+        if (estadoSel == "Nariño") {
+            arrayCiudades = ["Pasto", "Tumaco", "Ipiales", "La Cruz", "Santacruz"];
+        }
+        if (estadoSel == "Risaralda") {
+            arrayCiudades = ["Pereira", "Dos Quebradas", "La Virginia", "Belén de Umbría", "Guática"];
+        }
+        if (estadoSel == "Quindio") {
+            arrayCiudades = ["Armenia", "Calarcá", "Circasia", "Cordoba", "Filandia"];
+        }
+
+        /* Venezuela */
+        if (estadoSel == "Carabobo") {
+            arrayCiudades = ["Valencia", "Naguanagua", "Puerto Cabello", "Tocuyito", "Guacara"]
+        }
+
+        if (estadoSel == "Anzoategui") {
+            arrayCiudades = ["Barcelona", "Puerto La Cruz", "Anaco", "El Tigre", "Lechería"]
+        }
+
+        if (estadoSel == "Zulia") {
+            arrayCiudades = ["Maracaibo", "Cabimas", "Ciudad Ojeda", "La Concepción", "Machiques"]
+        }
+
+        if (estadoSel == "Apure") {
+            arrayCiudades = ["San Fernando", "Guasdualito", "Achaguas", "Bruzual", "Mantecal"]
+        }
+
+        if (estadoSel == "Mérida") {
+            arrayCiudades = ["Mérida", "El Vigia", "Ejido", "Lagunillas", "Nueva Bolivia"]
+        }
+
+        /* USA */
+        if (estadoSel == "Florida") {
+            arrayCiudades = ["Miami", "Forth Lauderdale", "Boca Ratón", "Tampa", "Orlando"]
+        }
+
+        if (estadoSel == "Nevada") {
+            arrayCiudades = ["Las Vegas", "Reno", "Carson City", "Henderson", "Sparks"]
+        }
+
+        if (estadoSel == "Iowa") {
+            arrayCiudades = ["Des Moines", "Iowa City", "Cedar Rapids", "Davenport", "Ames"]
+        }
+
+        if (estadoSel == "Kentucky") {
+            arrayCiudades = ["Luisville", "Lexington", "Fráncfort", "Paducah", "Hazard"]
+        }
+
+        if (estadoSel == "Colorado") {
+            arrayCiudades = ["Denver", "Colorado Springs", "Aspen", "Boulder", "Fort Collins"]
+        }
+
+        document.getElementById("ciudades").addEventListener("change", () => {
+            var ciudades = document.getElementById("ciudades");
+
+            ciudadSel = ciudades.options[ciudades.selectedIndex].text;
+
+            console.log("Ciudad seleccionada es " + ciudadSel);
+        })
+
+        var ciudades = document.getElementById("ciudades");
+        for (i = 0; i < arrayCiudades.length; i++) {
+            ciudades.options[i+1].text = arrayCiudades[i];
+        }
+
     }
 
     function getBandera(seleccionado) {
