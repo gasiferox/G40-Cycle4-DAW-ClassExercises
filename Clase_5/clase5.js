50f90b1552c6ca3ad80d0cc6e831c0b8e09903bb
Gustavo Romero <gasifero@gmail.com>
2021-11-03 18:49:49 -0500

    Clase 5 en version previa, codigo parcial a mitad de clase

---
+++ Clase_5/clase5.js
@@ -0,0 +1,43 @@
+/* let numeroA = 10
+let numeroB = 10
+let numeroC = null
+let numeroD = null
+
+numeroC = numeroA++
+numeroD = ++numeroB
+
+console.log("A = " + numeroA)
+console.log("B = " + numeroB)
+console.log("C = " + numeroC)
+console.log("D = " + numeroD)
+
+
+let numero = null
+for(numero = 1; numero < 11; numero ++)
+{
+    console.log("Numero " + numero)
+} */
+
+window.onload = function()
+{
+
+    document.getElementById("ejecutar").addEventListener("click", function(e)
+    {
+        var areaGenerada = document.getElementById("generado")
+        var areaGenerada2 = document.getElementById("generado2")
+
+
+        areaGenerada.innerHTML = "for( let x = " + document.getElementById("inicio").value + " ; x <=  " + document.getElementById("final").value + "; x++ ) {\n" + " console.log( '-' + x );\n" + "}\n\n"
+
+        areaGenerada2.innerHTML="";
+        for(let y = document.getElementById("inicio").value; y <= document.getElementById("final").value; y++) {
+            /* console.log(x) */
+            areaGenerada2.innerHTML += y + "\n";
+        }
+
+        /* for (let x = document.getElementById("Inicio").value; x <= document.getElementById("Final").value; x++) {
+            areagenerada.value += x + "\n"
+        } */
+
+    })
+}
---
+++ Clase_5/servidor.js
@@ -0,0 +1,25 @@
+const PORT = 8000
+
+const express = require('express')
+
+const app = express()
+
+/* const path = require('path') */
+
+const {createReadStream} = require('fs')
+
+//obtener resp
+app.get('/', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'})
+
+    createReadStream('Clase_5/clase5.html').pipe(res);
+
+    /* res.end('Mensaje del servidor') */
+})
+
+app.post("/upload", (req,res) => {
+    res.send("Respuesta del servidor")
+})
+
+
+app.listen(PORT, () => console.log('Servidor corriendo en el puerto ' + PORT))
