932f6a871edacbefd2d2aea7a299dad3e683d012
Gustavo Romero <gasifero@gmail.com>
2021-11-03 22:05:51 -0500

    Clase 5 codigo completo, funcionando la carga de archivos en la carpeta upload

--- Clase_5/servidor.js
+++ Clase_5/servidor.js
@@ -4,21 +4,46 @@ const express = require('express')
 
 const app = express()
 
-/* const path = require('path') */
+const fs = require('fs')
+
+const multer = require("multer")
+
+const path = require('path')
 
 const {createReadStream} = require('fs')
 
+const HTML_CONTENT_TYPE = 'text/html'
+
 //obtener resp
 app.get('/', (req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.writeHead(200, {'Content-Type': HTML_CONTENT_TYPE})
 
     createReadStream('Clase_5/clase5.html').pipe(res);
 
     /* res.end('Mensaje del servidor') */
 })
 
+let objMulter = multer({dest: "./public/upload"});
+
+app.use(objMulter.any())/
+
+app.use(express.static("./public"));
+
 app.post("/upload", (req,res) => {
-    res.send("Respuesta del servidor")
+    /* res.send("Respuesta del servidor") */
+
+    let oldName = req.files[0].path;// Obtener el nombre
+    // Agrega el sufijo original al nuevo nombre
+    let newName = req.files[0].path + path.parse(req.files[0].originalname).ext;
+    fs.renameSync(oldName, newName);// Cambiar el nombre del archivo
+    
+    res.send({
+      err: 0,
+      url:
+        "http://localhost:8000/upload/" +
+        req.files[0].filename + path.parse(req.files[0].originalname).ext // La ruta de vista previa de la imagen
+    });
+  
 })
 
 
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # G40 Cycle4 DAW Class Excercises
+
+## <u>Nota para Clase 5</u>
+
+Cuando vayan a correr el código no olvidar instalar el `multer`.
+
+```
+npm i multer --save
+```
